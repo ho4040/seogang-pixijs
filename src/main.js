96783632318d6b9de087d727b4a4a198aec31dca
seogang-game <seogang@seogang.com>
2018-10-08 10:53:45 +0900

    keyboard event added

--- src/main.js
+++ src/main.js
@@ -2,15 +2,30 @@ const app = new PIXI.Application({width:640, height:480});
 document.body.appendChild(app.view);
 
 let g = new PIXI.Graphics;
-g.beginFill(0xFF0000, 1);
+g.beginFill(0x0000FF, 1);
 g.drawCircle(0, 0, 30);
 g.endFill();
+g.x = app.renderer.width/2;
+g.y = app.renderer.height/2;
 
 app.stage.addChild(g);
 
-let g_t = 0;
+let keyState = {};
+let speed = 5;
 app.ticker.add(delta=>{
-    g_t += delta;
-    g.x = Math.sin(g_t * 0.05)*100 + app.renderer.width/2;
-    g.y = app.renderer.height/2;
+    if( !!keyState['ArrowUp'] )
+        g.y -= speed;
+    if( !!keyState['ArrowDown'] )
+        g.y += speed;
+    if( !!keyState['ArrowLeft'] )
+        g.x -= speed;
+    if( !!keyState['ArrowRight'] )
+        g.x += speed;
+})
+
+document.addEventListener('keydown', function(evt){    
+    keyState[evt.code] = true;
+})
+document.addEventListener('keyup', function(evt){
+    keyState[evt.code] = false;
 })
