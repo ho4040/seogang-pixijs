705f63c255db40a79a130a4742470a55544d5de3
zero choi <ho4040@nooslab.com>
2018-10-21 23:35:25 +0900

    gulp build command

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
 const browserSync = require('browser-sync');
 const gulp = require('gulp');
+const fs = require('fs');
+var ncp = require('ncp').ncp;
+
+gulp.task('build', function(){
+    const folders = [
+        'release',
+        'release/',
+        'release/static',
+        'release/libs',
+    ]
+
+    folders.forEach(dir=>{
+        if(!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+            console.log('📁  folder created:', dir);    
+        }
+    })
+
+    gulp.src("src/**").pipe(gulp.dest('release/'))    
+    gulp.src("resources/**").pipe(gulp.dest('release/static'))
+   
+    ncp("node_modules/pixi.js", "release/libs/pixi.js", {clobber:true}, function(err){})
+    ncp("node_modules/howler", "release/libs/howler", {clobber:true}, function(err){})
+})
 
 gulp.task('serve', function(){
     initBrowserSync('src');
